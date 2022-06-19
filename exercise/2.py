df1379394b5452507da3e49a1e7ed6c7f0713f2d
Raj ahir_007 <107246980+Rajahir007@users.noreply.github.com>
2022-06-19 13:26:25 +0530

    Add files via upload

---
+++ exercise/2.py
@@ -0,0 +1,13 @@
+#Print the following pattern
+
+row = 10
+# start: 1
+# stop: row+1 (range never include stop number in result)
+# step: 1
+# run loop 5 times
+for i in range(1, row + 1, 1):
+    # Run inner loop i+1 times
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    # empty line after each row
+    print("")
---
+++ exercise/6.py
@@ -0,0 +1,11 @@
+# Count the total number of digits in a number
+
+num = 75869
+count = 0
+while num != 0:
+    
+    num = num // 10
+
+    
+    count = count + 1
+print("Total digits are:", count)
